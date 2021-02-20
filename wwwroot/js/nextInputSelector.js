114f56328c58b521c2f1f1404c342ec51e77b9ba
Sean <seanedwards97@gmail.com>
2021-02-20 16:11:23 +0000

    Update nextInputSelector file

---
+++ wwwroot/js/nextInputSelector.js
@@ -0,0 +1,32 @@
+﻿// Taken from https://stackoverflow.com/questions/15595652/focus-next-input-once-reaching-maxlength-value
+
+function nextInputSelector(e) {
+
+    var target = e;
+    var maxLength = target.maxLength;
+    var myLength = target.value.length;
+    if (myLength >= maxLength) {
+        var next = target;
+        console.log(next);
+        while (next = next.nextElementSibling) {
+            if (next == null)
+                break;
+            if (next.tagName.toLowerCase() === "input") {
+                next.focus();
+                break;
+            }
+        }
+    }
+    // Move to previous field if empty (user pressed backspace)
+    else if (myLength === 0) {
+        var previous = target;
+        while (previous = previous.previousElementSibling) {
+            if (previous == null)
+                break;
+            if (previous.tagName.toLowerCase() === "input") {
+                previous.focus();
+                break;
+            }
+        }
+    }
+}
